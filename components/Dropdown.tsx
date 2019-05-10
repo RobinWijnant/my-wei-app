4b535f62d3cc40d88a8c8aba3fbab6de13daeee9
Robin Wijnant <robin@wijnant.me>
2019-05-10 17:17:51 +0200

    Dropdown

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
+import {FlatList, ListRenderItemInfo, Modal, StyleProp, StyleSheet, Text, View, ViewStyle} from 'react-native';
 import DropdownUI from "./DropdownUI";
+import {capitalize} from "../utils/utils";
 
 interface Props {
   defaultValue: string;
@@ -11,40 +12,57 @@ interface Props {
 
 interface State {
   selectedValue: string;
+  modalVisible: boolean;
 }
 
 export default class Dropdown extends React.Component<Props, State> {
   state: State = {
     selectedValue: this.props.defaultValue,
+    modalVisible: false,
   };
 
-  private onSelect(selectedValue: string) {
+  private selectItem(selectedValue: string) {
     this.setState({selectedValue: selectedValue});
     this.props.onSelect(this.state.selectedValue);
+    this.closeModal();
   }
 
   private openModal() {
+    this.setState({modalVisible: true});
+  }
 
+  private closeModal() {
+    this.setState({modalVisible: false});
   }
 
   render() {
-    let index = 0;
-    const data = [
-      { key: index++, section: true, label: 'Fruits' },
-      { key: index++, label: 'Red Apples' },
-      { key: index++, label: 'Cherries' },
-      { key: index++, label: 'Cranberries', accessibilityLabel: 'Tap here for cranberries' },
-      // etc...
-      // Can also add additional custom keys which are passed to the onChange callback
-      { key: index++, label: 'Vegetable', customKey: 'Not a fruit' }
-    ];
-
     return (
       <View style={this.props.style}>
         <DropdownUI
-          value={this.state.selectedValue}
-          onTouched={() => this.openModal()}
+          value={capitalize(this.state.selectedValue)}
+          onTouchEnd={this.openModal.bind(this)}
         />
+        <Modal
+          animationType={'fade'}
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={this.closeModal.bind(this)}>
+          <View
+            style={styles.dimmedBackground}
+            onTouchEnd={this.closeModal.bind(this)}>
+            <View style={styles.box}>
+              <FlatList
+                data={[{key: 'week'},{key: 'day'}]}
+                renderItem={({item}: ListRenderItemInfo<{key: string}>) => { return (
+                  <Text
+                    onPress={() => this.selectItem(item.key)}
+                    style={styles.listItem}
+                  >{capitalize(item.key)}</Text>
+                )}}
+              />
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -52,4 +70,26 @@ export default class Dropdown extends React.Component<Props, State> {
 
 const styles = StyleSheet.create({
   container: {},
+
+  dimmedBackground: {
+    backgroundColor: '#00000070',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+
+  box: {
+    backgroundColor: '#FFFFFF',
+    width: '100%',
+    padding: 20,
+    borderRadius: 10
+  },
+
+  listItem: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 16,
+  },
 });
